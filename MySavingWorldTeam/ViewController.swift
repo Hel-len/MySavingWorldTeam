088dacb9870d451850796c7fcc2f5b92e62933d3
Lena <eveforeve@yandex.ru>
2021-11-10 12:27:55 +0500

    Initial Commit

---
+++ MySavingWorldTeam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySavingWorldTeam
+//
+//  Created by Елена Дранкина on 10.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
