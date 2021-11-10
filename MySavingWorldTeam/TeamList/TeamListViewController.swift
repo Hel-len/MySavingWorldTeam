7fdedfcca318b3d059f1e21370fc1be0b1c74153
Lena <eveforeve@yandex.ru>
2021-11-10 12:32:24 +0500

    Del storyboard

---
+++ MySavingWorldTeam/TeamList/TeamListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TeamListViewController.swift
+//  MySavingWorldTeam
+//
+//  Created by Елена Дранкина on 10.11.2021.
+//
+
+import UIKit
+
+class TeamListViewController: UIViewController {
+    
+}
